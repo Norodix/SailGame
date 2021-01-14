4d6203488e2300065da8c92512ab58f183f5c775
Norodix <peceli.bence@gmail.com>
2021-01-14 21:12:13 +0100

    Status update, fullscreen, git ignore exports

--- World.gd
+++ World.gd
@@ -13,5 +13,6 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("Reset"):
+		get_tree().reload_current_scene()
