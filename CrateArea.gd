8c4f408a372eb8e39af7339b5a96196d88f68912
Norodix <peceli.bence@gmail.com>
2021-07-17 14:30:13 +0200

    New sounds for item pickups

--- CrateArea.gd
+++ CrateArea.gd
@@ -8,11 +8,19 @@ var maxRadius = 5000
 var minRadius = 1000
 var frameCount = 0
 
+var sounds
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	frameCount = $AnimatedSprite.frames.get_frame_count("default")
 	$AnimatedSprite.frame = randi() % frameCount
+	
+	sounds = [$Break_wood, \
+			$Break_wood, \
+			$Break_metal, \
+			$Break_glass]
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,6 +37,9 @@ func _on_CrateArea_body_entered(body):
 		var angle = randf()*2*PI
 		self.position += Vector2(cos(angle)*r, sin(angle)*r)
 	
+	#print(get_node("Break_" + sounds[$AnimatedSprite.frame]))
+	#print($AnimatedSprite.frame)
+	sounds[$AnimatedSprite.frame].play()
+	#$Break.play()
 	$AnimatedSprite.frame = randi() % frameCount
-	$Break.play()
 
