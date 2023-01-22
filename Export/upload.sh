399a63c2da29e53f694d82c26e15139eb43deed0
Norodix <peceli.bence@gmail.com>
2023-01-22 14:43:24 +0100

    Add butler export

--- Export/upload.sh
+++ Export/upload.sh
@@ -13,3 +13,7 @@ zip -r SailGame_html.zip SailGame_html/*
 #Upload webgl version and exe
 rsync -aP -e 'ssh -p 5566' ./SailGame_html ./SailGame_win.zip ./SailGame_linux.zip ./SailGame_android/SailGame.apk norodix@norodixserver.duckdns.org:/home/norodix/SailGame
 
+butler push "SailGame_html.zip"              "Norodix/sailgame:html"
+butler push "SailGame_linux.zip"             "Norodix/sailgame:linux"
+butler push "SailGame_win.zip"               "Norodix/sailgame:windows"
+butler push "SailGame_android/SailGame.apk"  "Norodix/sailgame:android"
