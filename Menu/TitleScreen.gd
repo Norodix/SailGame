42cacd06b14cb4b071bed30fae713e17ffef4d9c
Norodix <peceli.bence@gmail.com>
2023-07-04 22:02:10 +0200

    Add patreon branding

--- Menu/TitleScreen.gd
+++ Menu/TitleScreen.gd
@@ -20,3 +20,14 @@ func _on_Tutorial_pressed():
 func _on_Credits_pressed():
 	get_tree().change_scene("res://Menu/Credits.tscn")
 	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	get_tree().quit(0)
+	pass # Replace with function body.
+
+
+
+func _on_Patreon_pressed():
+	OS.shell_open("https://www.patreon.com/Norodix")
+	pass # Replace with function body.
